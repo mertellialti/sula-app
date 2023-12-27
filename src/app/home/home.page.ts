33c4e9f8a46263df5582f1be7c155c24e6a8315e
mertellialti <127337111+mertellialti@users.noreply.github.com>
2023-12-27 16:49:13 +0300

    slider image addded

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
 import { FooterService } from '../services/footer.service';
+import { IonicSlides } from '@ionic/angular';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +19,23 @@ export class HomePage {
 
   footerColumn2: any[];
 
+  swiperModules = [IonicSlides];
+  slidingImgs = ['../../assets/imgs/6.jpeg','../../assets/imgs/7.jpeg','../../assets/imgs/8.jpeg']
+  
+  swiperOptions: SwiperOptions = {
+    slidesPerView: 1.2, // Display 1.2 images per view
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false, // Enable autoplay even when the user interacts with the Swiper
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Allow pagination bullets to be clickable
+    },
+  };
+  
+
   constructor(
     private readonly menuSrv: MenuService,
     private readonly footerSrv: FooterService
@@ -25,6 +44,7 @@ export class HomePage {
     this.womanMenu = this.menuSrv.womanMenu;
     this.footerColumn1 = this.footerSrv.footerColumn1
     this.footerColumn2 = this.footerSrv.footerColumn2
+    
   }
 
 }
