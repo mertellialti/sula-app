2eb016ce51758733374015dfe7e87ce0ea82bed4
mertellialti <127337111+mertellialti@users.noreply.github.com>
2024-01-03 02:33:58 +0300

    cart initialized

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, Input, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 import { IonicModule } from '@ionic/angular';
 import { IonicSlides } from '@ionic/angular';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -14,13 +16,37 @@ import { IonicSlides } from '@ionic/angular';
   ],
   standalone: true
 })
-export class ProductComponent  implements OnInit {
+export class ProductComponent implements OnInit {
 
   @Input() product: any; // Input for product data
   swiperModules = [IonicSlides];
-    
-  constructor() { }
+  protected cartVal: number = 0
 
-  ngOnInit() {}
+  constructor(    
+    private readonly productSrv: ProductService
+  ) { }
 
+  async ngOnInit() {
+    this.cartVal = await this.checkInChart(this.product.id);
+  }
+
+  protected async checkInChart(id: string) {
+    const x = (await Preferences.get({ key: id })).value;    
+    return (x) ? parseInt(x) : 0;
+    // return this.cartSrv.isInCart(id);
+  }
+
+  protected async updateItem(id: string, amount: number) {
+    const x = (await Preferences.get({ key: id })).value;
+    console.log('x: ', x);
+    if (x) {
+      const newValue = parseInt(x) + amount;      
+      if (newValue <= 0) { await Preferences.remove({ key: id }); this.cartVal = 0 }
+      else { await Preferences.set({ key: id, value: newValue.toString() }); this.cartVal = newValue }
+    } else {
+      await Preferences.set({ key: id, value: amount.toString() });
+      this.cartVal = amount;
+    }
+    // this.cartSrv.updateItem(id,amount);
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,10 +7,10 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   productMenTShirt = [
-    { name: 'JUICE', price: 99, images: ['../../assets/imgs/man/t-shirt/0.jpeg', '../../assets/imgs/man/t-shirt/1.jpeg'] },
-    { name: 'STAMP Records', price: 89, images: ['../../assets/imgs/man/t-shirt/2.jpeg'] },
-    { name: 'Chase The Carrot', price: 94.99, images: ['../../assets/imgs/man/t-shirt/3.jpeg'] },
-    { name: 'Good Vibes', price: 94.99, images: ['../../assets/imgs/man/t-shirt/4.jpeg'] },
+    { id: 0, name: 'JUICE', price: 99, images: ['../../assets/imgs/man/t-shirt/0.jpeg', '../../assets/imgs/man/t-shirt/1.jpeg'] },
+    { id: 1, name: 'STAMP Records', price: 89, images: ['../../assets/imgs/man/t-shirt/2.jpeg'] },
+    { id: 2, name: 'Chase The Carrot', price: 94.99, images: ['../../assets/imgs/man/t-shirt/3.jpeg'] },
+    { id: 3, name: 'Good Vibes', price: 94.99, images: ['../../assets/imgs/man/t-shirt/4.jpeg'] },
   ]
 
   constructor() {
